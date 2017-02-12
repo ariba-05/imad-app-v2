52e543c8d547839076366d187e007e09e407bcc1
oreanroy <oreanroy@gmail.com>
2017-02-12 13:18:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('Article one requested will be serves shrotly');
+});
+
+app.get('/article-two',function(req,res)
+{
+    res.send('Article two requested will be served shortly');
+});
+app.get('/article-three',function(req,res)
+{
+    res.send('Article three requested wil be served shortly');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
