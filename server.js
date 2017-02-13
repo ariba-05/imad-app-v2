d9d896b4c0636fe6571148aab73c560b56bb4c8e
oreanroy <oreanroy@gmail.com>
2017-02-13 23:54:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,14 +91,14 @@ app.get('/:article-Name',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res)
-{
-    res.send('Article two requested will be served shortly');
-});
-app.get('/article-three',function(req,res)
-{
-    res.send('Article three requested wil be served shortly');
-});
+//app.get('/article-two',function(req,res)
+//{
+  //  res.send('Article two requested will be served shortly');
+//});
+//app.get('/article-three',function(req,res)
+//{
+  //  res.send('Article three requested wil be served shortly');
+//});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
