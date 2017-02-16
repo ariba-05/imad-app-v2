b88e3eaaa5a7441604732d9330ed6f6c017a1c0d
oreanroy <oreanroy@gmail.com>
2017-02-16 21:57:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get('/:articleName',function(req,res){
 //{
   //  res.send('Article three requested wil be served shortly');
 //});
+var counter=0;
 app.get('/counter', function(req,res){
     counter=counter+1;
     res.send(counter.tostring());
