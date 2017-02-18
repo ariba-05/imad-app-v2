dce9e6828d282ebe3a6e481d4580fb1ff7ce2ecd
oreanroy <oreanroy@gmail.com>
2017-02-18 18:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,22 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick = function() {
 //make a respose to counter 
+var request=new XMLHttpRequest();
 //capture the response store in a variable
+request.onreadystatechange= function(){
+ if(request.readyState===XMLHttpRequest.Done){
+     //take some anction
+     if(request.status===200){
+         var counter=request.responeText;
+         span.innerHTML=counter.toString();
+    
 //render the variable in correct span
 counter=counter+1;
 var span=document.getElementById('count');
 span.innerHTML =counter.toString();
+}
+}
+};
+request.open('GET','http://oreanroy.imad.hasura-app.io/counter',true);
+request.send(null);
 };
