555dd3a860b4703ad7813d3137d5e927ccb50887
oreanroy <oreanroy@gmail.com>
2017-02-21 19:03:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,16 @@ span.innerHTML = counter.toString();
 request.open('GET','http://oreanroy.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function()
+{
+    var names=['name1','name2','nmae3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li>'+names[i]+'</li>';
+    }var ul=document.getElementById('name list');
+    ul.innerHtml= list;
+};
