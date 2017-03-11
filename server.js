b10df90b76a129ed140e3e003eff5456414a0488
oreanroy <oreanroy@gmail.com>
2017-03-11 23:57:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/test-db', function(req, res)
     res.status(500).send(err.toString());
     }
     else{
-        res.send(JSON.Stringify(result));
+        res.send(JSON.Stringify(result.rows));
         
     }
     
