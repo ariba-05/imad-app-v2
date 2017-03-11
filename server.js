962ed8f002df1fe77cd80d42404230e301d46fc0
oreanroy <oreanroy@gmail.com>
2017-03-11 23:09:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+
+var config = {
+    user: 'oreanroy',
+    datbase: 'oreanroy',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+    
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -84,6 +95,22 @@ var articles ={
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new pool(config);
+app.get('/test-db', function(req, res)
+{   //make a select request
+    //return a response with results
+    pool.query('SELECT * FROM test', function(err, result){
+    if(err){
+    res.status(500).send(err.toString());
+    }
+    else{
+        res.send(JSON.Stringify(result));
+        
+    }
+    
+    });
 });
 
 var counter=0;
